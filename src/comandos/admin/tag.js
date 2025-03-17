e4e3d95aa8c7cffcc4f7f5ffbdd88534da5f02d5
Krampus B. <tiobarryx@gmail.com>
2025-03-17 17:10:42 +0100

    Actualizar tag.js

--- src/comandos/admin/tag.js
+++ src/comandos/admin/tag.js
@@ -5,13 +5,44 @@ module.exports = {
   description: "Para mencionar a todos",
   commands: ["tag", "t"],
   usage: `${PREFIX}hidetag motivo`,
-  handle: async ({ fullArgs, sendText, socket, remoteJid, sendReact }) => {
-    const { participants } = await socket.groupMetadata(remoteJid);
 
+  handle: async ({ fullArgs, sendText, sendReply, socket, remoteJid, sendReact, quotedMessage }) => {
+    // Obtener los participantes del grupo
+    const { participants } = await socket.groupMetadata(remoteJid);
     const mentions = participants.map(({ id }) => id);
 
+    // Enviar reacción
     await sendReact("📎");
 
-    await sendText(`\n\n${fullArgs}`, mentions);
+    // Texto del mensaje
+    const mensaje = fullArgs.trim() || "Atención, grupo!";
+    const link = "https://www.instagram.com/_vasquezemmanuel/";
+
+    // Datos para la previsualización
+    const contextInfo = {
+      isForwarded: true,
+      forwardingScore: 2,
+      externalAdReply: {
+        title: "Soky Bot",
+        body: "Operacion Marshall",
+        thumbnailUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/9.png",
+        sourceUrl: link,
+      },
+    };
+
+    // Si el mensaje es una respuesta a otro, responder directamente
+    if (quotedMessage) {
+      await socket.sendMessage(remoteJid, {
+        text: mensaje,
+        mentions,
+        contextInfo,
+      }, { quoted: quotedMessage });
+    } else {
+      await socket.sendMessage(remoteJid, {
+        text: mensaje,
+        mentions,
+        contextInfo,
+      });
+    }
   },
 };

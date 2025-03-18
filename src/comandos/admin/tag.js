b7015766be7c54ac39f572b1006c606f02732a20
Krampus B. <tiobarryx@gmail.com>
2025-03-18 01:48:31 +0100

    Actualizar tag.js

--- src/comandos/admin/tag.js
+++ src/comandos/admin/tag.js
@@ -5,44 +5,13 @@ module.exports = {
   description: "Para mencionar a todos",
   commands: ["tag", "t"],
   usage: `${PREFIX}hidetag motivo`,
-
-  handle: async ({ fullArgs, sendText, sendReply, socket, remoteJid, sendReact, quotedMessage }) => {
-    // Obtener los participantes del grupo
+  handle: async ({ fullArgs, sendText, socket, remoteJid, sendReact }) => {
     const { participants } = await socket.groupMetadata(remoteJid);
+
     const mentions = participants.map(({ id }) => id);
 
-    // Enviar reacción
     await sendReact("📎");
 
-    // Texto del mensaje
-    const mensaje = fullArgs.trim() || "Atención, grupo!";
-    const link = "https://www.instagram.com/KrampusOM";
-
-    // Datos para la previsualización
-    const contextInfo = {
-      isForwarded: true,
-      forwardingScore: 2,
-      externalAdReply: {
-        title: "Krampus OM bot",
-        body: "Operacion Marshall",
-        thumbnailUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/9.png",
-        sourceUrl: link,
-      },
-    };
-
-    // Si el mensaje es una respuesta a otro, responder directamente
-    if (quotedMessage) {
-      await socket.sendMessage(remoteJid, {
-        text: mensaje,
-        mentions,
-        contextInfo,
-      }, { quoted: quotedMessage });
-    } else {
-      await socket.sendMessage(remoteJid, {
-        text: mensaje,
-        mentions,
-        contextInfo,
-      });
-    }
+    await sendText(`\n\n${fullArgs}`, mentions);
   },
 };

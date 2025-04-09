84ed88b3c4ff95d2f7c24e383b5c1fb9bbbfcaa7
Krampus B. <tiobarryx@gmail.com>
2025-04-09 18:03:26 +0200

    Update ping.js

--- src/comandos/member/ping.js
+++ src/comandos/member/ping.js
@@ -2,17 +2,37 @@ const { PREFIX } = require("../../krampus");
 
 module.exports = {
   name: "ping",
-  description: "Verificar se o bot está online",
+  description: "Verifica si el bot está online.",
   commands: ["ping"],
   usage: `${PREFIX}ping`,
-  handle: async ({ sendReply, sendReact }) => {
+  handle: async ({ sendReact, socket, remoteJid }) => {
     const startTime = Date.now();
     await sendReact("👻");
     const endTime = Date.now();
     const latency = endTime - startTime;
+
     const speed = latency.toFixed(2) + "ms";
-    await sendReply(`Velocidad de respuesta: ${speed}\n> Krampus OM bot`);
+    let statusEmoji = "🟢";
+    if (latency > 300) {
+      statusEmoji = "🔴";
+    } else if (latency > 100) {
+      statusEmoji = "🟡";
+    }
+
+    const fakeQuoted = {
+      key: {
+        remoteJid: remoteJid,
+        fromMe: false,
+        id: "FAKE-QUOTE-PING",
+        participant: "0@s.whatsapp.net",
+      },
+      message: {
+        conversation: "Poche bot\n by Krampus",
+      },
+    };
+
+    await socket.sendMessage(remoteJid, {
+      text: `${statusEmoji} *Bot activo*\n📡 Velocidad de respuesta: *${speed}*`,
+    }, { quoted: fakeQuoted });
   },
 };
-
-
